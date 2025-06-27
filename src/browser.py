1980c483d807121596a8e9f43c178cb8c80fbc90
caioxsf <caio.nobile@hotmail.com>
2025-06-26 23:00:23 -0300

    fix: add mode headless for open google chrome

--- src/browser.py
+++ src/browser.py
@@ -11,7 +11,7 @@ class Browser:
     """
     def __init__(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-gpu')
